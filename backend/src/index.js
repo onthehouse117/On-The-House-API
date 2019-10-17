d5540d9378bb84f1f7bf413c3ed5a771f66303ce
raopg <prateek.rao.08@gmail.com>
2019-10-16 21:05:25 -0700

    Created basic express server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,11 @@
-/**  On-The-House API  */
+/* On-The-House API  */
+const express = require('express')
+
+const app = express()
+const port = process.env.port || 3000
+
+app.use(express.json)
+
+app.listen(port, () =>{
+    console.log('Server is up on port ' + port)
+})
