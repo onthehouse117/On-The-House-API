25e554e51418ea075e72d824e1d6122b7596b592
Bruce Lee <brucelee92371@gmail.com>
2019-10-17 22:01:05 -0700

    Add MongoDB URI / Added Environment Variables

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 // To run mongodb /Users/giorgio/mongodb/bin/mongod --dbpath=/Users/giorgio/mongodbdata --port=27017
 
 mongoose.connect('https://localhost:27018/on-the-house', {
+    //Username and Password in environment variables
+    //mongodb+srv://dbuser:password@cluster0-evyen.mongodb.net/test?retryWrites=true&w=majority
     useNewUrlParser: true,
     useCreateIndex: true
 })
