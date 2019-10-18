9703b5a185f981da7a09b48940f7dd1874d7bc3f
Giorgio Atanasov <giorgioatanasov34@yahoo.com>
2019-10-17 17:55:58 -0700

    added first get request

---
+++ backend/routers/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/test', (req, res) => {
+    res.send('This is my test router')
+})
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 /* On-The-House API  */
+const user = require('../routers/user');
 const express = require('express')
 
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json)
+app.use(user);
 
 app.listen(port, () =>{
     console.log('Server is up on port ' + port)
