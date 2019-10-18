60581d759ea4f13727795b22489631f49e526aaa
raopg <prateek.rao.08@gmail.com>
2019-10-17 18:31:36 -0700

    Created test endpoint to make initial connection

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 
-router.get('/test', (req, res) => {
-    res.send('This is my test router')
-})
+
 
 
 module.exports = router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,16 @@ const user = require('../routers/user');
 const express = require('express')
 
 const app = express()
-const port = process.env.port || 3000
+const port = 3000
 
-app.use(express.json)
+//app.use(express.json)
 app.use(user);
+
+/* Basic test endpoint. Note: If app using JSON as means of communication, this endpoint will NOT work */
+app.post('/test', (req, res) => {
+    console.log('Ran test endpoint')
+    res.send('This is my test router')
+})
 
 app.listen(port, () =>{
     console.log('Server is up on port ' + port)
