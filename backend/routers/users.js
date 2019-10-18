ebc4d4c4f47630419f9ef003d734cb644bc94a42
raopg <prateek.rao.08@gmail.com>
2019-10-18 01:08:09 -0700

    Fixed a bug in user registration

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -5,6 +5,8 @@ const User = require('../models/user');
 router.post('/users', async (req, res) => {
     console.log(req.body);
     const newUser = new User(req.body);
+    
+    await newUser.save() // We will save the document in our hashing function after it is implemented. For now, save the doc asynchronously
     try {
         res.status(201).send(newUser);
     }
