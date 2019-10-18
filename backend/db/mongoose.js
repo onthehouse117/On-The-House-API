bcd0697022fccf23e1d3bfd724ba82a01c70d434
raopg <prateek.rao.08@gmail.com>
2019-10-17 20:00:05 -0700

    Changed MongoDB connection URL to include a collection

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // To run mongodb /Users/giorgio/mongodb/bin/mongod --dbpath=/Users/giorgio/mongodbdata --port=27017
 
-mongoose.connect('https://localhost:27017', {
+mongoose.connect('https://localhost:27018/on-the-house', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
--- backend/routers/users.js
+++ backend/routers/users.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 
-router.post('/createUser', async (res, req) => {
+router.post('/users', async (req, res) => {
     console.log(req.body);
     const newUser = new User(req.body);
     try {
         res.status(201).send(newUser);
     }
     catch (e) {
-        res.status(404).send(e);
+        res.status(400).send(e);
     }
 })
 
