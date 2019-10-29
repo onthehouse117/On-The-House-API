478fd09c99c673e51555fdad1e1110d9b86370dd
raopg <prateek.rao.08@gmail.com>
2019-10-28 23:00:13 -0700

    Updated index.js to use post and admin routers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 /* On-The-House API  */
-const userRouter = require('./routers/users');
 require('./db/mongoose');
 const express = require('express');
+/* All the routers*/
+const userRouter = require('./routers/users');
+const postRouter = require('./routers/posts');
+const adminRouter = require('./routers/admin');
 const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+
 app.use(userRouter);
+app.use(postRouter);
+app.use(adminRouter);
+
 app.use(cors());
 
 /* Basic test endpoint. Note: If app using JSON as means of communication, this endpoint will NOT work */
