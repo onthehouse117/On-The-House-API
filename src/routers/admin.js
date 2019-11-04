359d257af075ddaad9471951c68d84b4f921c373
Giorgio Atanasov <giorgioatanasov34@yahoo.com>
2019-11-03 17:02:04 -0800

    created deletePost functionality for Admin and fixed other code bugs

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -3,6 +3,7 @@ const {auth, admin} = require("../middleware/auth");
 const router = new express.Router();
 const User = require("../models/user");
 const multer = require("multer");
+const Post = require('../models/post');
 
 /** Admin registration endpoint
  *  @desc Endpoint to register an admin to our database. Along with email and password, the user must provide a secret code.
@@ -49,6 +50,34 @@ router.post('/admin/deleteUser', auth, admin, async (req, res) =>{
         res.status(200).send(user)
     } catch(e){
         console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+router.post('/admin/deletePost', auth, admin, async (req, res) => {
+    try {
+        var options = {} // id, title, author
+        if (req.body._id) {
+            options._id = req.body._id
+        }
+        if (req.body.title) {
+            options.title = req.body.title
+        }
+        if (req.body.author) {
+            options.author = req.body.author
+            const user = await User.findOne({_id: options.author})
+            if (!user) {
+                throw new Error('User does not exist')
+            }
+        }
+        if (!options) {
+            throw new Error('Must provide one of following options: postId, postTitle, or author')
+        }
+        const post = await Post.findOne(options)
+        await post.remove()
+        res.status(200).send(post)
+    }
+    catch (e){
         res.status(400).send(e)
     }
 })
